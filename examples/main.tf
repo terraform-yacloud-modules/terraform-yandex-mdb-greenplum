a775cd022c084ec47c982ab5f41e60d2c6684250
Anton Patsev <patsev.anton@gmail.com>
2024-07-29 18:56:13 +0600

    Исправил ошибки tflint

--- examples/main.tf
+++ examples/main.tf
@@ -21,9 +21,9 @@ module "network" {
 module "greenplum_cluster" {
   source = "../"
 
-  network_id          = module.network.vpc_id
-  subnet_id           = module.network.private_subnets_ids[0]
-  zone_id             = "ru-central1-a"
+  network_id = module.network.vpc_id
+  subnet_id  = module.network.private_subnets_ids[0]
+  zone_id    = "ru-central1-a"
 
   cluster_name        = "test-greenplum"
   cluster_description = "Test Greenplum cluster"
@@ -31,9 +31,9 @@ module "greenplum_cluster" {
   assign_public_ip    = true
   greenplum_version   = "6.22"
 
-  master_host_count   = 2
-  segment_host_count  = 5
-  segment_in_host     = 1
+  master_host_count  = 2
+  segment_host_count = 5
+  segment_in_host    = 1
 
   master_resources_preset = "s3-c8-m32"
   master_disk_size        = 93
@@ -46,8 +46,8 @@ module "greenplum_cluster" {
   access_web_sql = true
 
   greenplum_config = {
-    max_connections                   = 395
-    gp_workfile_compression           = "false"
+    max_connections         = 395
+    gp_workfile_compression = "false"
   }
 
   user_name     = "admin_user"
